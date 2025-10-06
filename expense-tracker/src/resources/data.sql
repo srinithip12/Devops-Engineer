07931fe443a15d07b07354213d956315780c7ad5
Srinithi Periyasamy <srinithi.p12@gmail.com>
2025-10-06 21:46:10 +0530

    Update and rename data.sql (optional seed data) to data.sql

---
+++ expense-tracker/src/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO expenses (name, category, amount, date) VALUES ('Lunch', 'Food', 150.00, '2025-10-06');
+INSERT INTO expenses (name, category, amount, date) VALUES ('Taxi', 'Transport', 320.00, '2025-10-05');
+INSERT INTO expenses (name, category, amount, date) VALUES ('Groceries', 'Food', 780.50, '2025-09-30');
+
